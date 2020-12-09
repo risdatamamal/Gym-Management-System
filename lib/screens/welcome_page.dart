5e47d17613dfe3c1b2a6b5651be45330507597d7
Indrajith <68956752+uldindrajith@users.noreply.github.com>
2020-12-09 12:30:38 +0530

    Welcome Screen Created
    Welcome Screen has been developed. No functinality yet. Just the UI.
    App name and icons changed.

---
+++ lib/screens/welcome_page.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery.of(context).size.height,
+          padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 50.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    'WELCOME',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w900,
+                      fontSize: 30.0,
+                      decoration: TextDecoration.underline,
+                      decorationStyle: TextDecorationStyle.wavy,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Text(
+                    'My GYM Manager enables you to manage your GYM related activities at your finger tips!',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.grey[700],
+                      fontSize: 15.0,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                        'assets/images/gym_coach_trainer_fitness-512.png'),
+                  ),
+                ),
+              ),
+              Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(
+                      top: 3.0,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Colors.black,
+                        ),
+                        top: BorderSide(
+                          color: Colors.black,
+                        ),
+                        left: BorderSide(
+                          color: Colors.black,
+                        ),
+                        right: BorderSide(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      height: 60.0,
+                      onPressed: () {},
+                      color: Color(0xFFC9C7F1),
+                      shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                            color: Colors.black,
+                          ),
+                          borderRadius: BorderRadius.circular(50.0)),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(
+                      top: 3.0,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(50.0),
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Colors.black,
+                        ),
+                        top: BorderSide(
+                          color: Colors.black,
+                        ),
+                        left: BorderSide(
+                          color: Colors.black,
+                        ),
+                        right: BorderSide(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      height: 60.0,
+                      onPressed: () {},
+                      color: Colors.purple[200],
+                      elevation: 0.0,
+                      shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                            color: Colors.black,
+                          ),
+                          borderRadius: BorderRadius.circular(50.0)),
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
