068e8bdc42a645e89710edefc7fbe2fd05b0bdc4
Indrajith <68956752+uldindrajith@users.noreply.github.com>
2020-12-26 18:06:14 +0530

    Income & Expense are now can be added to the DB
    Incomes and Expenses can be added to the DB. But not yet able to view from the app.

--- lib/screens/income_expenses/add_income.dart
+++ lib/screens/income_expenses/add_income.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:my_gym_manager/config/palette.dart';
 import 'package:my_gym_manager/widgets/custom_app_bar2.dart';
 import 'package:my_gym_manager/widgets/make_input.dart';
@@ -9,8 +12,16 @@ class AddIncome extends StatefulWidget {
 }
 
 class _AddIncomeState extends State<AddIncome> {
+  final referenceDatabase = FirebaseDatabase.instance;
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController amountController = TextEditingController();
+  final TextEditingController detailsController = TextEditingController();
+  final TextEditingController dateController = TextEditingController()
+    ..text = 'Please select a Date.';
   @override
   Widget build(BuildContext context) {
+    final ref = referenceDatabase.reference();
     return Scaffold(
       backgroundColor: Palette.primaryColor,
       appBar: CustomAppBar2(Icons.arrow_back_ios, () {
@@ -56,18 +67,69 @@ class _AddIncomeState extends State<AddIncome> {
                       MakeInput(
                         label: 'Title',
                         obscureText: false,
+                        controllerID: titleController,
                       ),
-                      MakeInput(
-                        label: 'Date',
-                        obscureText: false,
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Date',
+                            style: TextStyle(
+                              fontSize: 15.0,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black87,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5.0,
+                          ),
+                          TextField(
+                            controller: dateController,
+                            enabled: false,
+                            decoration: InputDecoration(
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: 0.0,
+                                horizontal: 10.0,
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.grey[400],
+                                ),
+                              ),
+                              border: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                  color: Colors.grey[400],
+                                ),
+                              ),
+                            ),
+                          ),
+                          RaisedButton(
+                            child: Text('Pick a Date'),
+                            onPressed: () {
+                              showDatePicker(
+                                context: context,
+                                initialDate: DateTime.now(),
+                                firstDate: DateTime(2001),
+                                lastDate: DateTime(2100),
+                              ).then((_dateTime) {
+                                setState(() {
+                                  dateController.text = DateFormat('yyyy-MM-dd')
+                                      .format(_dateTime);
+                                });
+                              });
+                            },
+                          ),
+                        ],
                       ),
                       MakeInput(
                         label: 'Amount',
                         obscureText: false,
+                        controllerID: amountController,
                       ),
                       MakeInput(
                         label: 'Income Details',
                         obscureText: false,
+                        controllerID: detailsController,
                       ),
                     ],
                   ),
@@ -85,12 +147,18 @@ class _AddIncomeState extends State<AddIncome> {
               ),
               child: FlatButton(
                 onPressed: () => {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => AddIncome(),
-                    ),
-                  ),
+                  ref.child(auth.currentUser.uid).child('Income').push().set(
+                    {
+                      'Title': titleController.text,
+                      'Amount': amountController.text,
+                      'Income_Details': detailsController.text,
+                      'Date': dateController.text,
+                    },
+                  ).asStream(),
+                  titleController.clear(),
+                  amountController.clear(),
+                  detailsController.clear(),
+                  dateController.clear(),
                 },
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
